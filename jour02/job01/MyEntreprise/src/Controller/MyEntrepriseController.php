b284d501d3d63e34142d8edc629154f0a018b741
solenn-massot <solennmassot@laplateforme.io>
2020-09-15 14:04:06 +0200

    Projet finis

--- jour02/job01/MyEntreprise/src/Controller/MyEntrepriseController.php
+++ jour02/job01/MyEntreprise/src/Controller/MyEntrepriseController.php
@@ -9,17 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class MyEntrepriseController extends AbstractController
 {
     /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+
+        return $this->render('myEntreprise/home.html.twig', [
+            'controller_name' => 'MyEntrepriseController',
+        ]);
+    }
+    /**
      * @Route("/myentreprise", name="myEntreprise")
      */
     public function index()
     {
         $projets = $this->getDoctrine()
-        ->getRepository(Projets::class)
-        ->findAll();
+            ->getRepository(Projets::class)
+            ->findAll();
 
         return $this->render('myEntreprise/index.html.twig', [
             'controller_name' => 'MyEntrepriseController',
             'projets' => $projets,
         ]);
+    }
+
+    /**
+     * @Route("/myentreprise/projet/{id}", name="project_page")
+     */
+    public function projectPage($id)
+    {
+        $projet = $this->getDoctrine()->getRepository(Projets::class)->findOneBy(['id' => $id]);
+        if (!$projet) {
+            // Si aucun article n'est trouvé, nous créons une exception
+            throw $this->createNotFoundException('Le projet n\'existe pas');
+        }
+        // Si l'article existe nous envoyons les données à la vue
+        return $this->render('myEntreprise/projectPage.html.twig', compact('projet'));
     }
 }
--- jour02/job01/MyEntreprise/src/DataFixtures/ProjectFixtures.php
+++ jour02/job01/MyEntreprise/src/DataFixtures/ProjectFixtures.php
@@ -10,10 +10,10 @@ class ProjectFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($count = 0; $count < 20; $count++) {
+        for ($count = 0; $count < 10; $count++) {
             $projet = new Projets();
-            $projet->setTitle("Titre " . $count);
-            $projet->setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at enim vel leo tempus congue nec vel ipsum. Etiam sed urna ut orci rhoncus tincidunt id id tortor. Quisque molestie nisi sed ex venenatis vehicula. Sed molestie magna magna, a scelerisque diam iaculis et. Curabitur et mi et est finibus convallis. Sed eu nunc viverra, sagittis sem sed, bibendum purus. Etiam mi eros, facilisis id arcu ut, finibus lacinia est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nisi dolor, sed vestibulum purus luctus vitae. Sed a metus nec massa semper convallis vel sit amet neque. Curabitur at ornare dui. Vivamus eu efficitur nisi.
+            $projet->setTitle("Article n° " . $count);
+            $projet->setContent("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at enim vel leo tempus congue nec vel ipsum. Etiam sed urna ut orci rhoncus tincidunt id id tortor. Quisque molestie nisi sed ex venenatis vehicula. Sed molestie magna magna, a scelerisque diam iaculis et. Curabitur et mi et est finibus convallis. Sed eu nunc viverra, sagittis sem sed, bibendum purus. Etiam mi eros, facilisis id arcu ut, finibus lacinia est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nisi dolor, sed vestibulum purus luctus vitae. Sed a metus nec massa semper convallis vel sit amet neque. Curabitur at ornare dui. Vivamus eu efficitur nisi.
 
             Suspendisse ac varius est. Proin quis massa a ex lacinia porttitor id ut libero. In odio sem, condimentum quis efficitur tincidunt, rutrum sit amet odio. Vestibulum posuere diam eget est ullamcorper pharetra nec a tortor. Etiam condimentum nunc id metus viverra posuere. Nulla faucibus dolor rutrum ante pulvinar, vel cursus orci sollicitudin. Donec vitae odio faucibus, consequat urna a, varius libero. Vivamus ut lorem nec orci maximus malesuada nec semper nibh. Donec consectetur massa elit, sed rutrum neque ullamcorper in. Nunc dictum tellus sed arcu convallis aliquet. Nullam tempor ultricies elit quis eleifend.
             
@@ -21,7 +21,7 @@ class ProjectFixtures extends Fixture
             
             Donec molestie molestie tortor. Phasellus vel dictum elit, et hendrerit justo. Suspendisse a massa non velit accumsan sodales. Morbi placerat id lacus vitae facilisis. Mauris rutrum congue ipsum, eu ornare magna suscipit quis. Cras sed massa id quam placerat elementum. Curabitur porttitor mauris at vulputate sollicitudin. Vestibulum imperdiet felis nec est viverra bibendum. Suspendisse sagittis dui ex, id bibendum diam mattis vitae. Ut dignissim lacinia libero, ac posuere dolor. Donec ac leo quis orci hendrerit fringilla. Sed vulputate congue quam id scelerisque. Phasellus porta tellus sed placerat tincidunt. Nam vulputate eu lorem eget suscipit.
             
-            Sed vel nulla eu dui condimentum malesuada. Mauris tristique quis augue non placerat. Duis sed arcu sed mauris fringilla tempor. Mauris sed eros tincidunt, gravida erat vel, tincidunt mauris. Fusce euismod libero nec viverra maximus. Nulla tempus ipsum enim, et eleifend ex scelerisque in. Integer euismod mauris imperdiet dui placerat elementum. Phasellus elit felis, rhoncus vel mauris venenatis, suscipit tristique libero. Maecenas luctus vestibulum porttitor. Phasellus sollicitudin viverra velit ac pulvinar.");
+            Sed vel nulla eu dui condimentum malesuada. Mauris tristique quis augue non placerat. Duis sed arcu sed mauris fringilla tempor. Mauris sed eros tincidunt, gravida erat vel, tincidunt mauris. Fusce euismod libero nec viverra maximus. Nulla tempus ipsum enim, et eleifend ex scelerisque in. Integer euismod mauris imperdiet dui placerat elementum. Phasellus elit felis, rhoncus vel mauris venenatis, suscipit tristique libero. Maecenas luctus vestibulum porttitor. Phasellus sollicitudin viverra velit ac pulvinar.</p>");
             $projet->setImage("https://source.unsplash.com/random/800x600");
             $projet->setCreatedAt(new \DateTime('now' , new \DateTimeZone('Europe/Berlin') ));
             $manager->persist($projet);
